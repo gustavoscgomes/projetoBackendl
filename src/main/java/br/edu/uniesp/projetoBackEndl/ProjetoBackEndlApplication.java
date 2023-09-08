b129b3ad0eab64e33f0f45ff30a32aadbebb2179
Gustavo Gomes <2022211790006@iesp.edu.brgit status>
2023-09-08 15:10:53 -0300

    primeiro commit

---
+++ src/main/java/br/edu/uniesp/projetoBackEndl/ProjetoBackEndlApplication.java
@@ -0,0 +1,13 @@
+package br.edu.uniesp.projetoBackEndl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackEndlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackEndlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/uniesp/projetoBackEndl/ProjetoBackEndlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.uniesp.projetoBackEndl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackEndlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
